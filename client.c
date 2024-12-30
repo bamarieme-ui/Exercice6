a270d2c3a7a518478cb118db7beb13d675a9692e
bamarieme-ui <bamaremelamine@gmail.com>
2024-12-30 13:59:19 +0000

    Update client.c

--- client.c
+++ client.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
     const char* ip_address = argv[1];
     int port = atoi(argv[2]);
 
-    // Créer le socket
+   
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("Erreur de création du socket\n");
         return -1;
@@ -24,27 +24,26 @@ int main(int argc, char* argv[])
     serv_addr.sin_port = htons(port);
 
     // Convertir l'adresse IP en format binaire
-    if (inet_pton(AF_INET, ip_address, &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, ip_address, &serv_addr.sin_addr) <= 0)
+    {
         printf("Adresse IP invalide\n");
         return -1;
     }
 
-    // Connecter au serveur
+   
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("Échec de connexion\n");
         return -1;
     }
 
     while (1) {
-        // Recevoir le compteur du serveur
+       
         read(sock, &msg, sizeof(msg));
         printf("Reçu du serveur: %d\n", msg.compteur);
-
-        // Incrémenter le compteur et attendre une seconde
+       
         msg.compteur++;
         sleep(1);
 
-        // Envoyer le compteur incrémenté au serveur
         send(sock, &msg, sizeof(msg), 0);
         printf("Envoyé au serveur: %d\n", msg.compteur);
     }
